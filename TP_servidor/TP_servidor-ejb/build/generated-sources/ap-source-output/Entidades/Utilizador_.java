0073b65c9e51b1830cd0d80792ac85c400a23bd6
SCrolem <darkxield@gmail.com>
2019-01-11 02:29:32 +0000

    rosinha

--- TP_servidor/TP_servidor-ejb/build/generated-sources/ap-source-output/Entidades/Utilizador_.java
+++ TP_servidor/TP_servidor-ejb/build/generated-sources/ap-source-output/Entidades/Utilizador_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-14T14:26:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-11T02:20:51")
 @StaticMetamodel(Utilizador.class)
 public class Utilizador_ { 
 
